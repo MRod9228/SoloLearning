665143affa6e79cf1b34ffc2dedd3974bea67e46
Mario Rodriguez <72200540+MRod9228@users.noreply.github.com>
2021-11-05 12:00:44 -0400

    Once again at it

---
+++ 100_Days_of_Code_Python_Projects/day_7_hangman_1_start.py
@@ -0,0 +1,23 @@
+import random
+
+#Step 1 
+
+word_list = ["Olivia", "Emma", "Ava", "Charlotte", "Sophia", "Amelia", "Isabella", "Mia", "Evelyn", "Harper"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = ''
+
+chosen_word = random.choice(word_list)
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+print('You will be guessing TOP 10 female names')
+
+guess = input('Please guess a LETTER: ').lower()
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+for letter in chosen_word:
+    if letter == guess:
+        print('Right')
+    else:
+        print('Wrong')
+
+
+
